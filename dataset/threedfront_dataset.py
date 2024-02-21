78a160e125316f019d7ce4afe30b70647f6ee2ba
ymxlzgy <zgyddzyx@163.com>
2024-02-21 18:48:29 +0100

    fix two bug in the 3dfront dataloader

--- dataset/threedfront_dataset.py
+++ dataset/threedfront_dataset.py
@@ -386,7 +386,7 @@ class ThreedFrontDatasetSceneGraph(data.Dataset):
                 ordered_feats = []
                 for inst in instances_order:
                     clip_feats_in_instance = inst == clip_feats_order
-                    ordered_feats.append(clip_feats_ins[:-1][clip_feats_in_instance])
+                    ordered_feats.append(clip_feats_ins[clip_feats_in_instance])
                 if self.use_scene_rels:
                     ordered_feats.append(clip_feats_ins[-1][np.newaxis,:]) # should be room's feature
                 clip_feats_ins = list(np.concatenate(ordered_feats, axis=0))
@@ -400,9 +400,10 @@ class ThreedFrontDatasetSceneGraph(data.Dataset):
             feats_rel = {}
             obj_cat = []
             with torch.no_grad():
-                for i in range(num_cat - 1):
+                for i in range(num_cat):
                     obj_cat.append(self.get_key(self.classes, cat_ids[i]))
                 if self.use_scene_rels:
+                    obj_cat.pop()
                     obj_cat.append('room') # TODO check
                 text_obj = clip.tokenize(obj_cat).to('cuda')
                 feats_ins = self.cond_model.encode_text(text_obj).detach().cpu().numpy()
